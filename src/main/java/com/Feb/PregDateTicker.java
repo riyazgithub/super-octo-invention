e3e7dd76d61e40f27c1848d4323505a54a3bdc19
Riyaz Ahmed Hassan Afthab <rafthab@salesforce.com>
2021-02-04 21:58:12 -0800

    adding week tracker for pregnancy

---
+++ src/main/java/com/Feb/PregDateTicker.java
@@ -0,0 +1,36 @@
+package com.Feb;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class PregDateTicker {
+
+  public static void main(String[] args) {
+	Date date = new Date();
+	long timeMilli = 1612502900100l; // start time Feb 4
+	System.out.println("Current Time "+timeMilli);
+
+	long currentTime = date.getTime();
+//	currentTime = 1619101849174l;
+
+
+	Calendar calendar = Calendar.getInstance();
+	calendar.setTimeInMillis(timeMilli);
+
+	int mYear = calendar.get(Calendar.YEAR);
+	int mMonth = calendar.get(Calendar.MONTH);
+	int mDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+	System.out.println("Year "+mYear+" mMonth "+mMonth+" mday "+mDay);
+	long difference = currentTime-timeMilli;
+	long perDayMilliSecs = 60*60*24*1000;
+
+	int startWeeks = 24;
+	int numberofDays = (int) (difference / perDayMilliSecs);
+	int numberOfWeeks =  (numberofDays / 7);
+	System.out.println("Number of Days "+numberofDays);
+	System.out.println("Current Week "+(numberOfWeeks + startWeeks));
+
+  }
+
+}
