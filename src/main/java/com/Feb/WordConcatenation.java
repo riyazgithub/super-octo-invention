7a663e341c5a566182ea5705d2d8b4765917abd0
Riyaz Ahmed Hassan Afthab <rafthab@salesforce.com>
2021-02-25 14:35:22 -0800

    Moving on

--- src/main/java/com/Feb/WordConcatenation.java
+++ src/main/java/com/Feb/WordConcatenation.java
@@ -3,21 +3,41 @@ package com.Feb;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+// Copied
 
 public class WordConcatenation {
+
   public static List<Integer> findWordConcatenation(String str, String[] words) {
+	Map<String, Integer> wordFrequencyMap = new HashMap<>();
+	for (String word : words)
+	  wordFrequencyMap.put(word, wordFrequencyMap.getOrDefault(word, 0) + 1);
+
 	List<Integer> resultIndices = new ArrayList<Integer>();
-	HashMap<Character, Integer> characterIntegerHashMap = new HashMap<>();
-	Integer matches = populateHM(characterIntegerHashMap,words);
-	populateIndices(str,characterIntegerHashMap, resultIndices);
+	int wordsCount = words.length, wordLength = words[0].length();
+
+	for (int i = 0; i <= str.length() - wordsCount * wordLength; i++) {
+	  Map<String, Integer> wordsSeen = new HashMap<>();
+	  for (int j = 0; j < wordsCount; j++) {
+		int nextWordIndex = i + j * wordLength;
+		// get the next word from the string
+		String word = str.substring(nextWordIndex, nextWordIndex + wordLength);
+		if (!wordFrequencyMap.containsKey(word)) // break if we don't need this word
+		  break;
+
+		wordsSeen.put(word, wordsSeen.getOrDefault(word, 0) + 1); // add the word to the 'wordsSeen' map
+
+		// no need to process further if the word has higher frequency than required
+		if (wordsSeen.get(word) > wordFrequencyMap.getOrDefault(word, 0))
+		  break;
+
+		if (j + 1 == wordsCount) // store index if we have found all the words
+		  resultIndices.add(i);
+	  }
+	}
+
 	return resultIndices;
-  }
-
-  private static void populateIndices(String str, HashMap<Character,Integer> characterIntegerHashMap, List<Integer> resultIndices) {
-  }
-
-  private static void populateIndices(String str, HashMap<Character,Integer> characterIntegerHashMap, List<Integer> resultIndices, Integer matches) {
-
   }
 
   private static Integer populateHM(HashMap<Character,Integer> charFrequencyMap, String[] words) {
@@ -34,4 +54,10 @@ public class WordConcatenation {
 	return matches;
   }
 
+  public static void main(String[] args) {
+    String[] words = {"cat","fox"};
+	System.out.println("Words Outside "+words.length);
+	System.out.println(WordConcatenation.findWordConcatenation("catfoxcat", words));
+	System.out.println(WordConcatenation.findWordConcatenation("catcatfoxfox", words));
+  }
 }
