77314af1f2d8c8674761bfc4b761f540b7e66f5f
Riyaz H A <rafthab@riyazha-ltm.internal.salesforce.com>
2021-03-22 10:11:48 -0700

    maxStack max rating

---
+++ src/main/java/com/Mar/BrowserRating.java
@@ -0,0 +1,23 @@
+package com.Mar;
+
+import com.helper.maxStack;
+
+public class BrowserRating {
+    public static void main(String[] args) {
+        maxStack maxStack = new maxStack(7);
+        maxStack.push(5);
+        maxStack.push(0);
+        maxStack.push(2);
+        maxStack.push(4);
+        maxStack.push(6);
+        maxStack.push(3);
+        maxStack.push(10);
+
+        System.out.println("Max Rating "+ maxStack.maxRating());
+
+        maxStack.pop();
+        System.out.println("Clicking back button");
+        System.out.println("Maximum Rating "+ maxStack.maxRating());
+
+    }
+}
